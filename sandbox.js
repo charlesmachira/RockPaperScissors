1b688d7d823c74cd3ee146204e578e2068ed6aa5
Charles Machira <charlesmachira2@gmail.com>
2022-12-11 22:36:51 +0300

    js added
    X#	modified:   sandbox.js

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+function getComputerChoice(){
+    //random choice between rock, paper and scissors
+    //return computerChoice
+}
+function getPlayerChoice(){
+    //prompt user for input: either rock, paper, or scissors
+    //return playerChoice
+}
+function playRound(computerChoice, playerChoice){
+    //rock > scissors
+    //paper > scissors
+    //scissors > rock
+}
